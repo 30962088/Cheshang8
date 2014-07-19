0df970e7d1de5869a52e187932828443ac0fca54
wiiliam <poolk001@qq.com>
2014-07-20 02:37:59 +0800

    update

---
+++ src/com/cheshang8/app/UserInfoActivity.java
@@ -0,0 +1,18 @@
+package com.cheshang8.app;
+
+import com.cheshang8.app.fragment.TabIndexFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class UserInfoActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_info_layout);
+	
+	}
+
+
+}
