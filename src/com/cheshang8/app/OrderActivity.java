a97a6a41ca5aac9eb0d92089447bcccb02dcdc07
wiiliam <poolk001@qq.com>
2014-07-20 01:45:24 +0800

    update

---
+++ src/com/cheshang8/app/OrderActivity.java
@@ -0,0 +1,25 @@
+package com.cheshang8.app;
+
+import com.cheshang8.app.fragment.TabIndexFragment;
+import com.cheshang8.app.fragment.TabZoneFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class OrderActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.order_layout);
+		
+	}
+	
+	
+
+}
